b9e8e283dd56fbcfed59d5eb71996b5421b68ebe
Marcus Caisey <marcus@teckna.com>
2020-06-25 17:44:52 +0100

    Add Item model and show all items on home page

--- lists/tests.py
+++ lists/tests.py
@@ -1,17 +1,40 @@
+import pytest
+
 from .models import Item
 
 
 class TestHome:
+    @pytest.mark.django_db
     def test_uses_home_template(self, client):
         response = client.get("/")
         assert "lists/home.html" in (t.name for t in response.templates)
 
+    @pytest.mark.django_db
+    def test_GET_doesnt_save_item(self, client):
+        client.get("/")
+        assert Item.objects.count() == 0
+
+    @pytest.mark.django_db
     def test_can_save_POST_request(self, client):
-        response = client.post("/", data={"item_text": "A new list item"})
+        client.post("/", data={"item_text": "A new list item"})
 
         items = Item.objects.all()
         assert items.count() == 1
-        assert items.first.text == "A new list item"
+        assert items.first().text == "A new list item"
 
-        assert "lists/home.html" in (t.name for t in response.templates)
-        assert b"A new list item" in response.content
+    @pytest.mark.django_db
+    def test_redirects_after_POST(self, client):
+        response = client.post("/", data={"item_text": "A new list item"})
+
+        assert response.status_code == 302
+        assert response.url == "/"
+
+    @pytest.mark.django_db
+    def test_displays_all_items(self, client):
+        Item.objects.create(text="item 1")
+        Item.objects.create(text="item 2")
+
+        response = client.get("/")
+
+        assert b"item 1" in response.content
+        assert b"item 2" in response.content
