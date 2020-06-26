12a680b5fdd7a1621133229f43be0b6601e2366b
Marcus Caisey <marcus@teckna.com>
2020-06-26 13:03:22 +0100

    Replace assert helper with wait helper to remove sleep calls

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -2,7 +2,10 @@ import time
 
 import pytest
 from selenium import webdriver
+from selenium.common import exceptions as selenium_exceptions
 from selenium.webdriver.common.keys import Keys
+
+MAX_WAIT = 10
 
 
 @pytest.fixture(scope="session")
@@ -14,21 +17,29 @@ def browser():
 
 
 @pytest.fixture
-def assert_row_in_todo_table(browser):
+def wait_for_row_in_todo_table(browser):
     """
     A function which checks if a row with the given text is in the to-do list
     table.
     """
 
-    def assert_row_in_todo_table(text):
-        table = browser.find_element_by_id("to-do_items")
-        rows = table.find_elements_by_tag_name("tr")
-        assert text in [row.text for row in rows]
+    def wait_for_row_in_todo_table(text):
+        start = time.time()
+        while True:
+            try:
+                table = browser.find_element_by_id("to-do_items")
+                rows = table.find_elements_by_tag_name("tr")
+                assert text in [row.text for row in rows]
+                return
+            except (AssertionError, selenium_exceptions.WebDriverException):
+                if time.time() - start > MAX_WAIT:
+                    raise
+                time.sleep(0.5)
 
-    return assert_row_in_todo_table
+    return wait_for_row_in_todo_table
 
 
-def test_can_start_a_list_and_retrieve_it_later(browser, live_server, assert_row_in_todo_table):
+def test_can_start_a_list_and_retrieve_it_later(browser, live_server, wait_for_row_in_todo_table):
     # Edith has heard about a cool new online to-do app. She goes to check out its homepage.
     browser.get(live_server.url)
 
@@ -47,9 +58,8 @@ def test_can_start_a_list_and_retrieve_it_later(browser, live_server, assert_row
     # When she hits enter, the page updates, and now the page lists "1: Buy peacock feathers" as an item in a to-do
     # list.
     input_.send_keys(Keys.ENTER)
-    time.sleep(1)
 
-    assert_row_in_todo_table("1: Buy peacock feathers")
+    wait_for_row_in_todo_table("1: Buy peacock feathers")
 
     # There is a still a text box inviting her to add another item. She enters "Use peacock feathers to make a fly"
     # (Edith is very methodical).
@@ -60,8 +70,8 @@ def test_can_start_a_list_and_retrieve_it_later(browser, live_server, assert_row
     input_.send_keys(Keys.ENTER)
     time.sleep(1)
 
-    assert_row_in_todo_table("1: Buy peacock feathers")
-    assert_row_in_todo_table("2: Use peacock feathers to make a fly")
+    wait_for_row_in_todo_table("1: Buy peacock feathers")
+    wait_for_row_in_todo_table("2: Use peacock feathers to make a fly")
 
     # Edith wonders whether the site will remember her list. Then she sees that the site has generated a unique URL for
     # her - there is some explanatory text to that affect.
