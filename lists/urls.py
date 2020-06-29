80b4874c106769bccff5095236e1a3715b3be869
Marcus Caisey <marcus@teckna.com>
2020-06-29 17:41:08 +0100

    Remove lists prefix from all lists app urls

--- lists/urls.py
+++ lists/urls.py
@@ -4,8 +4,7 @@ from . import views
 
 app_name = "lists"
 urlpatterns = [
-    path("", views.home_page, name="home"),
-    path("lists/new/", views.new_list, name="new-list"),
-    path("lists/<int:pk>/add_item/", views.add_item, name="add-item"),
-    path("lists/<int:pk>/", views.view_list, name="view-list"),
+    path("new/", views.new_list, name="new-list"),
+    path("<int:pk>/add_item/", views.add_item, name="add-item"),
+    path("<int:pk>/", views.view_list, name="view-list"),
 ]
--- superlists/urls.py
+++ superlists/urls.py
@@ -1,5 +1,8 @@
 from django.urls import include, path
 
+from lists import views as list_views
+
 urlpatterns = [
-    path("", include("lists.urls")),
+    path("", list_views.home_page, name="home"),
+    path("lists/", include("lists.urls")),
 ]
