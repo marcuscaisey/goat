034dafe92b8444b217e5187af71efbe4574d4378
Marcus Caisey <marcus@teckna.com>
2020-07-08 17:56:16 +0100

    Add outline for empty list item functional test

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -143,3 +143,21 @@ def test_layout(browser, live_server_url):
 
     input_ = browser.find_element_by_id("new_item_input")
     assert input_.location["x"] + input_.size["width"] / 2 == pytest.approx(512, abs=10)
+
+
+def test_cannot_add_empty_list_items(browser, live_server_url):
+    # Edith goes to the home page and accidentally tries to submit an empty list
+    # item. She hints Enter on the empty input box
+
+    # The home page refreshes and there is an error message saying that list
+    # items cannot be blank
+
+    # She tries again with some text for the item, which now works
+
+    # Perversely, she now decides to submit a second blank list item
+
+    # She receives a similar warning on the list page
+
+    # And she can correct it by filling some text in
+
+    pytest.fail("write me!")
