d1ecd4a63f7547ff17c53fae5b057183675434c6
Marcus Caisey <marcus@teckna.com>
2020-07-09 11:49:30 +0100

    Refactor item saving and retrieving test

--- tests/unit/apps/lists/test_models.py
+++ tests/unit/apps/lists/test_models.py
@@ -3,27 +3,13 @@ import pytest
 from lists.models import Item, List
 
 
-class TestListAndItem:
+class TestItem:
     @pytest.mark.django_db
-    def test_saving_and_retrieving_items(self):
-        list_ = List()
-        list_.save()
+    def test_saving_and_retrieving(self):
+        list_ = List.objects.create()
+        Item.objects.create(text="The first (ever) list item", list=list_)
 
-        first_item = Item()
-        first_item.text = "The first (ever) list item"
-        first_item.list = list_
-        first_item.save()
+        saved_item = Item.objects.first()
 
-        second_item = Item()
-        second_item.text = "Item the second"
-        second_item.list = list_
-        second_item.save()
-
-        saved_list = List.objects.first()
-        first_saved_item, second_saved_item = Item.objects.all()
-
-        assert saved_list == list_
-        assert first_saved_item.text == "The first (ever) list item"
-        assert first_saved_item.list == list_
-        assert second_saved_item.text == "Item the second"
-        assert second_saved_item.list == list_
+        assert saved_item.text == "The first (ever) list item"
+        assert saved_item.list == list_
