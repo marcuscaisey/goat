39a85408ca862a38b069762cf3c3b1996cc8ef54
Marcus Caisey <marcus@teckna.com>
2020-07-13 15:24:35 +0100

    Refactor wait_for_row_in_todo_table in terms of wait_for

--- tests/functional/conftest.py
+++ tests/functional/conftest.py
@@ -33,28 +33,20 @@ def live_server_url(live_server):
     return os.getenv("STAGING_SERVER", live_server.url)
 
 
-MAX_WAIT = 10
-
-
 @pytest.fixture
-def wait_for_row_in_todo_table():
+def wait_for_row_in_todo_table(wait_for):
     """
     A function which checks if a row with the given text is in the to-do list
     table.
     """
 
     def wait_for_row_in_todo_table(text, browser):
-        start = time.time()
-        while True:
-            try:
-                table = browser.find_element_by_id("to-do_items")
-                rows = table.find_elements_by_tag_name("tr")
-                assert text in [row.text for row in rows]
-                return
-            except (AssertionError, selenium_exceptions.WebDriverException):
-                if time.time() - start > MAX_WAIT:
-                    raise
-                time.sleep(0.5)
+        def row_in_todo_table():
+            table = browser.find_element_by_id("to-do_items")
+            rows = table.find_elements_by_tag_name("tr")
+            return text in [row.text for row in rows]
+
+        return wait_for(row_in_todo_table)
 
     return wait_for_row_in_todo_table
 
