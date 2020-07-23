c8c9b33041af93254aed4deee7a964c0d197bfba
Marcus Caisey <marcus@teckna.com>
2020-07-23 14:22:26 +0100

    Mock ItemForm in lists view tests

--- tests/unit/apps/lists/test_views.py
+++ tests/unit/apps/lists/test_views.py
@@ -14,6 +14,19 @@ def view_list_url_factory():
     return view_list_url_factory
 
 
+@pytest.fixture
+def mock_item_form(mocker):
+    """A mock ItemForm."""
+    return mocker.patch("lists.views.ItemForm", autospec=True)
+
+
+@pytest.fixture
+def mock_item_form_instance(mock_item_form):
+    """A mock ItemForm instance."""
+    return mock_item_form.return_value
+
+
+@pytest.mark.django_db
 class TestViewList:
     @pytest.fixture
     def view_list_url(self, list, view_list_url_factory):
@@ -30,15 +43,13 @@ class TestViewList:
         """Response to a GET request to the view list page for a list."""
         return client.get(view_list_url)
 
-    @pytest.mark.django_db
     def test_uses_list_template(self, get_response, assert_template_used, list_template):
+        assert get_response.status_code == 200
         assert_template_used(get_response, list_template)
 
-    @pytest.mark.django_db
     def test_passes_item_form_to_template(self, get_response):
         assert isinstance(get_response.context["form"], ItemForm)
 
-    @pytest.mark.django_db
     def test_passes_list_to_template(self, get_response, list):
         assert get_response.context["list"] == list
 
@@ -47,7 +58,6 @@ class TestViewList:
         """Response to a successful POST request to the view list page."""
         return client.post(view_list_url, {"text": "A new list item"})
 
-    @pytest.mark.django_db
     def test_can_save_a_POST_request_to_an_existing_list(self, success_response, list):
         saved_items = Item.objects
         assert saved_items.count() == 1
@@ -56,60 +66,32 @@ class TestViewList:
         assert saved_item.text == "A new list item"
         assert saved_item.list == list
 
-    @pytest.mark.django_db
     def test_redirects_to_list_view(self, success_response, view_list_url, assert_redirects):
         assert_redirects(success_response, view_list_url)
 
     @pytest.fixture
-    def empty_input_response(self, client, view_list_url):
+    def invalid_form_response(self, client, view_list_url, mock_item_form_instance):
         """
-        Response to a POST request to the view list page with invalid input.
+        A response to a POST request to the view list page with invalid input.
         """
-        return client.post(view_list_url, data={"text": ""})
+        mock_item_form_instance.is_valid.return_value = False
+        return client.post(view_list_url, {"text": "A new list item."})
 
-    @pytest.mark.django_db
-    def test_empty_input_renders_list_template(self, empty_input_response, assert_template_used, list_template):
-        assert empty_input_response.status_code == 200
-        assert_template_used(empty_input_response, list_template)
+    def test_invalid_form_called_with_data_and_list(self, invalid_form_response, mock_item_form, list):
+        mock_item_form.assert_called_with(invalid_form_response.wsgi_request.POST, list_=list)
 
-    @pytest.mark.django_db
-    def test_empty_input_passes_list_to_template(self, empty_input_response, list):
-        assert empty_input_response.context["list"] == list
+    def test_invalid_form_renders_list_template(self, invalid_form_response, assert_template_used, list_template):
+        assert invalid_form_response.status_code == 200
+        assert_template_used(invalid_form_response, list_template)
 
-    @pytest.mark.django_db
-    def test_empty_input_passes_form_to_template(self, empty_input_response, assert_form_is_instance_with_errors):
-        assert_form_is_instance_with_errors(empty_input_response.context["form"], ItemForm)
+    def test_invalid_form_passes_list_to_template(self, invalid_form_response, list):
+        assert invalid_form_response.context["list"] == list
 
-    @pytest.mark.django_db
-    def test_empty_list_items_arent_saved(self, empty_input_response):
-        assert Item.objects.count() == 0
+    def test_invalid_form_passes_form_to_template(self, invalid_form_response, mock_item_form_instance):
+        assert invalid_form_response.context["form"] == mock_item_form_instance
 
-    @pytest.fixture
-    def duplicate_item_response(self, client, view_list_url_factory, item):
-        """
-        Response to a POST request to the view list page with duplicate text
-        input.
-        """
-        return client.post(view_list_url_factory(item.list), data={"text": item.text})
-
-    @pytest.mark.django_db
-    def test_duplicate_list_item_renders_list_template(self, duplicate_item_response, assert_template_used):
-        assert duplicate_item_response.status_code == 200
-        assert_template_used(duplicate_item_response, "lists/list.html")
-
-    @pytest.mark.django_db
-    def test_duplicate_list_item_passes_list_to_template(self, duplicate_item_response, list):
-        assert duplicate_item_response.context["list"] == list
-
-    @pytest.mark.django_db
-    def test_duplicate_list_item_passes_form_to_template(
-        self, duplicate_item_response, assert_form_is_instance_with_errors
-    ):
-        assert_form_is_instance_with_errors(duplicate_item_response.context["form"], ItemForm)
-
-    @pytest.mark.django_db
-    def test_duplicate_list_items_arent_saved(self, duplicate_item_response):
-        assert Item.objects.count() == 1
+    def test_invalid_form_doesnt_save(self, invalid_form_response, mock_item_form_instance):
+        mock_item_form_instance.save.assert_not_called()
 
 
 @pytest.fixture
@@ -124,11 +106,9 @@ class TestHome:
         """Response to a GET request for the home page."""
         return client.get(home_url)
 
-    @pytest.mark.django_db
     def test_uses_home_template(self, get_response, assert_template_used, home_template):
         assert_template_used(get_response, home_template)
 
-    @pytest.mark.django_db
     def test_uses_item_form(self, get_response):
         assert isinstance(get_response.context["form"], ItemForm)
 
@@ -160,20 +140,22 @@ class TestNewList:
         assert_redirects(success_response, view_list_url_factory(list_))
 
     @pytest.fixture
-    def empty_input_response(self, client, new_list_url):
-        """Response to a POST request to the new list view with empty text input."""
-        return client.post(new_list_url, data={"text": ""})
+    def invalid_form_response(self, client, new_list_url, mock_item_form_instance):
+        """
+        A response to a POST request to the new list view with invalid input.
+        """
+        mock_item_form_instance.is_valid.return_value = False
+        return client.post(new_list_url, {"text": "A new list item."})
 
-    @pytest.mark.django_db
-    def test_empty_input_renders_home_template(self, empty_input_response, assert_template_used, home_template):
-        assert empty_input_response.status_code == 200
-        assert_template_used(empty_input_response, home_template)
+    def test_invalid_form_called_with_data(self, invalid_form_response, mock_item_form):
+        mock_item_form.assert_called_with(invalid_form_response.wsgi_request.POST)
 
-    @pytest.mark.django_db
-    def test_empty_input_passes_form_to_template(self, empty_input_response, assert_form_is_instance_with_errors):
-        assert_form_is_instance_with_errors(empty_input_response.context["form"], ItemForm)
+    def test_invalid_form_doesnt_save(self, invalid_form_response, mock_item_form_instance):
+        mock_item_form_instance.save.assert_not_called()
 
-    @pytest.mark.django_db
-    def test_empty_list_items_arent_saved(self, empty_input_response):
-        assert Item.objects.count() == 0
-        assert List.objects.count() == 0
+    def test_invalid_form_renders_home_template(self, invalid_form_response, assert_template_used, home_template):
+        assert invalid_form_response.status_code == 200
+        assert_template_used(invalid_form_response, home_template)
+
+    def test_invalid_form_passes_form_to_template(self, invalid_form_response, mock_item_form_instance):
+        assert invalid_form_response.context["form"] == mock_item_form_instance
